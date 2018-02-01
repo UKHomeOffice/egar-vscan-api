a8533f067da51ea3305d4e057aad05294d30d909
Bill Hart <Bill.Hart@civica.co.uk>
2018-02-01 16:26:52 +0000

    Amended build scripts

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# Push Docker image to Quay.io
 NAME="${1}"
 version=$(./scripts/bash/get-site-version.sh)
 
-docker tag $NAME:$version pipe.egarteam.co.uk/$NAME:$version
-docker push pipe.egarteam.co.uk/$NAME:$version
-
-docker tag $NAME:$version pipe.egarteam.co.uk/$NAME:latest
-docker push pipe.egarteam.co.uk/$NAME:latest
+docker login -u="ukhomeofficedigital+egar_robot" -p=${DOCKER_PASSWORD} quay.io
+docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:$version
+# docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:latest
+docker push quay.io/ukhomeofficedigital/$NAME:$version
+# docker push quay.io/ukhomeofficedigital/$NAME:lastest
