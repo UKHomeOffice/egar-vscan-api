2527b08b6c16fb234d525b7c05fc29336dcfb0c2
Bill Hart <Bill.Hart@civica.co.uk>
2018-02-02 14:48:48 +0000

    Testing change to deploy script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -5,6 +5,7 @@ version=$(./scripts/bash/get-site-version.sh)
 
 docker login -u="ukhomeofficedigital+egar_robot" -p=${DOCKER_PASSWORD} quay.io
 docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:$version
-# docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:latest
 docker push quay.io/ukhomeofficedigital/$NAME:$version
-# docker push quay.io/ukhomeofficedigital/$NAME:lastest
+
+docker tag $NAME:$versoin quay.io/ukhomeofficedigital/$NAME:latest
+docker push quay.io/ukhomeofficedigital/$NAME:lastest
